89fc3f7a0eb893cc3e05402c6f83356a6775ed07
samurai20000 <samurai20000___@___gmail.com>
2010-10-03 23:36:17 +0900

    インデントがおかしかったので修正

--- meddling_click.user.js
+++ meddling_click.user.js
@@ -6,110 +6,110 @@
 // ==/UserScript==
 
 (function() {
-		function MeddlingClick() {
-				this.init.apply(this, arguments);
-		}
+    function MeddlingClick() {
+        this.init.apply(this, arguments);
+    }
 
     MeddlingClick.prototype = {
-				init: function () {
-						var a = document.querySelectorAll('a, input, textarea, button, img');
-						this.links = {};
+        init: function () {
+            var a = document.querySelectorAll('a, input, textarea, button, img');
+            this.links = {};
 
-						for (var i = 0, j = a.length; i < j; i++) {
-								this.links[a[i]] = this.getElementPosition(a[i]);
-								this.links[a[i]].elm = a[i];
-						}
-				},
+            for (var i = 0, j = a.length; i < j; i++) {
+                this.links[a[i]] = this.getElementPosition(a[i]);
+                this.links[a[i]].elm = a[i];
+            }
+        },
 
-				getTargetLink: function (e) {
-						var pos        = this.getMousePosition(e);
-						var targetLink = null;
-						var min        = Infinity;
-						
-						for (var key in this.links) {
-								var link = this.links[key];
-								
-								var d = this.getDistance(pos, link);
+        getTargetLink: function (e) {
+            var pos        = this.getMousePosition(e);
+            var targetLink = null;
+            var min        = Infinity;
 
-								if (min > d && this.is_MeddlingArea(link.height, d)) { 
-										min = d;
-										targetLink = link;
-								}
-						}
-						
-						if (targetLink) {
-								this.focus(targetLink.elm);
-								this.click(targetLink.elm);
-						}
-				},
+            for (var key in this.links) {
+                var link = this.links[key];
 
-				getDistance: function(mouse_pos, link) {
-						var distance = Infinity;
-						if (mouse_pos.x < link.left) {
-								if (mouse_pos.y < link.top) {
-										distance = Math.sqrt(Math.pow(link.left - mouse_pos.x, 2) +
-																	Math.pow(link.top - mouse_pos.y, 2));
-								} else if (mouse_pos.y >= link.top && mouse_pos.y <= link.bottom) {
-										distance = Math.sqrt(Math.pow(link.left - mouse_pos.x, 2));
-								} else {
-										distance = Math.sqrt(Math.pow(link.left - mouse_pos.x, 2) +
-																	Math.pow(link.bottom - mouse_pos.y, 2));
-								}
-						} else if (mouse_pos.x >= link.left && mouse_pos.x <= link.right) {
-								if (mouse_pos.y < link.top) {
-										distance = Math.sqrt(Math.pow(link.top - mouse_pos.y, 2));
-								} else {
-										distance = Math.sqrt(Math.pow(link.bottom - mouse_pos.y, 2));
-								}
-						} else {
-								if (mouse_pos.y < link.top) {
-										distance = Math.sqrt(Math.pow(link.right - mouse_pos.x, 2) +
-																	Math.pow(link.top - mouse_pos.y, 2));
-								} else if (mouse_pos.y >= link.top && mouse_pos.y <= link.bottom) {
-										distance = Math.sqrt(Math.pow(link.right - mouse_pos.x, 2));
-								} else {
-										distance = Math.sqrt(Math.pow(link.right - mouse_pos.x, 2) +
-																			Math.pow(link.buttom - mouse_pos.y, 2));
-								}
-						}
-						return distance;
-				},
+                var d = this.getDistance(pos, link);
 
-				is_MeddlingArea: function(link_height, distance){
-						var area = Math.sqrt(Math.pow(link_height, 2) * 2);
-						return area >= distance ? true : false;
-				},
+                if (min > d && this.is_MeddlingArea(link.height, d)) { 
+                    min = d;
+                    targetLink = link;
+                }
+            }
 
-				click: function(elm) {
-						if (elm.href) location.href = elm.href;
-				},
+            if (targetLink) {
+                this.focus(targetLink.elm);
+                this.click(targetLink.elm);
+            }
+        },
 
-				focus: function(elm) {
-						elm.focus();
-				},
+        getDistance: function(mouse_pos, link) {
+            var distance = Infinity;
+            if (mouse_pos.x < link.left) {
+                if (mouse_pos.y < link.top) {
+                    distance = Math.sqrt(Math.pow(link.left - mouse_pos.x, 2) +
+                                         Math.pow(link.top - mouse_pos.y, 2));
+                } else if (mouse_pos.y >= link.top && mouse_pos.y <= link.bottom) {
+                    distance = Math.sqrt(Math.pow(link.left - mouse_pos.x, 2));
+                } else {
+                    distance = Math.sqrt(Math.pow(link.left - mouse_pos.x, 2) +
+                                         Math.pow(link.bottom - mouse_pos.y, 2));
+                }
+            } else if (mouse_pos.x >= link.left && mouse_pos.x <= link.right) {
+                if (mouse_pos.y < link.top) {
+                    distance = Math.sqrt(Math.pow(link.top - mouse_pos.y, 2));
+                } else {
+                    distance = Math.sqrt(Math.pow(link.bottom - mouse_pos.y, 2));
+                }
+            } else {
+                if (mouse_pos.y < link.top) {
+                    distance = Math.sqrt(Math.pow(link.right - mouse_pos.x, 2) +
+                                         Math.pow(link.top - mouse_pos.y, 2));
+                } else if (mouse_pos.y >= link.top && mouse_pos.y <= link.bottom) {
+                    distance = Math.sqrt(Math.pow(link.right - mouse_pos.x, 2));
+                } else {
+                    distance = Math.sqrt(Math.pow(link.right - mouse_pos.x, 2) +
+                                         Math.pow(link.buttom - mouse_pos.y, 2));
+                }
+            }
+            return distance;
+        },
 
-				getMousePosition: function (e) {
-						var pos = {
-								x: e.pageX + document.documentElement.scrollLeft,
-								y: e.pageY + document.documentElement.scrollTop
-						};
-						return pos;
-				},
+        is_MeddlingArea: function(link_height, distance){
+            var area = Math.sqrt(Math.pow(link_height, 2) * 2);
+            return area >= distance ? true : false;
+        },
 
-				getElementPosition: function (elm) {
-						var pos = elm.getBoundingClientRect();
-						return {
-								width  : Math.round(pos.width),
-								height : Math.round(pos.height),
-								left   : Math.round(pos.left),
-								top    : Math.round(pos.top),
-								right  : Math.round(pos.right),
-								bottom : Math.round(pos.bottom)
-						};
-				}
-		};
+        click: function(elm) {
+            if (elm.href) location.href = elm.href;
+        },
 
-		// main
+        focus: function(elm) {
+            elm.focus();
+        },
+
+        getMousePosition: function (e) {
+            var pos = {
+                x: e.pageX + document.documentElement.scrollLeft,
+                y: e.pageY + document.documentElement.scrollTop
+            };
+            return pos;
+        },
+
+        getElementPosition: function (elm) {
+            var pos = elm.getBoundingClientRect();
+            return {
+                width  : Math.round(pos.width),
+                height : Math.round(pos.height),
+                left   : Math.round(pos.left),
+                top    : Math.round(pos.top),
+                right  : Math.round(pos.right),
+                bottom : Math.round(pos.bottom)
+            };
+        }
+    };
+
+    // main
     var meddling_click = new MeddlingClick();
     document.addEventListener('mouseup', function(event) {meddling_click.getTargetLink(event)}, false);
 }());
